701717ed41fa52ca6bc3c995e251331452651027
Erik Nilsen <enilsen16@live.com>
2014-11-12 11:38:09 -0800

    binary search implementation

--- helpers.c
+++ helpers.c
@@ -6,14 +6,24 @@
  */
 bool search(int value, int values[], int n)
 {
-
   //While length of list > 0
+    int min = 0;
+    int max = n - 1;
+  while ( max > min ) {
+    int mid = (max + min) / 2;
     //Look at the middle
     //If number found, return true
+    if (values[mid] == value) {
+      return true;
     //Else if number is higher, search left
+  } else if ( values[mid] > value) {
+      max = mid - 1;
     //Else if number is lower, search right
-  //Recursively repeat
-
+  } else if (values[mid] < value) {
+      min = mid + 1;
+    }
+  }
+  return false;
 }
 
 /**
